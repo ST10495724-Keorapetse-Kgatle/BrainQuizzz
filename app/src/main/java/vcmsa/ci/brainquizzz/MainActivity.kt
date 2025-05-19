3c843bd8c4737274a8fc0b0b27b6486b7c9f54bb
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 14:39:42 +0200

    allows the user to exit the application

--- app/src/main/java/vcmsa/ci/brainquizzz/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/MainActivity.kt
@@ -25,6 +25,10 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent (this,quizkit::class.java)
             startActivity( intent)
         }
+        //Exits  the application
+        btnExit.setOnClickListener {
+            finish()
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
