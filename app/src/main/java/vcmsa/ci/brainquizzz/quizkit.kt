2d4c70c1e8b7628a24956ad41ef3ec7208a805b0
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 14:49:19 +0200

    added constrains

--- app/src/main/java/vcmsa/ci/brainquizzz/quizkit.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/quizkit.kt
@@ -1,12 +1,21 @@
 package vcmsa.ci.brainquizzz
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class quizkit : AppCompatActivity() {
+    //Declare
+    private lateinit  var question: TextView
+    private lateinit var feedBack: TextView
+    private lateinit var btnTrue: Button
+    private lateinit var btnFalse: Button
+    private lateinit var btnNext: Button
+    private lateinit var btnResult: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
