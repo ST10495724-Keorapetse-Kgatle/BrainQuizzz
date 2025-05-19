a8aecb4bc51a226b62c8d4b89fd63d2f1b0e38e2
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 14:36:47 +0200

    declare

--- app/src/main/java/vcmsa/ci/brainquizzz/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/MainActivity.kt
@@ -1,12 +1,16 @@
 package vcmsa.ci.brainquizzz
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+   // Declare
+    private  lateinit var  btnStart : Button
+    private lateinit var  btnExit : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
