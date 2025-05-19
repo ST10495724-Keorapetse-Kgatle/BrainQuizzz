ddca793055858decd3a76330ae1376d957b91651
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 15:29:30 +0200

    initialize

--- app/src/main/java/vcmsa/ci/brainquizzz/Score.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/Score.kt
@@ -18,6 +18,11 @@ class Score : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_score)
+        // Initialize
+        score = findViewById(R.id.score)
+        btnReview = findViewById(R.id.btnReview)
+        btnExit = findViewById(R.id.btnExit)
+        feedBackReview = findViewById(R.id.feedBackReview)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
