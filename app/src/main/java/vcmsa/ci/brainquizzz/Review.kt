0cf38086681900522e551dafb793ef0790fad5cf
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 15:49:51 +0200

    declare

--- app/src/main/java/vcmsa/ci/brainquizzz/Review.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/Review.kt
@@ -1,12 +1,18 @@
 package vcmsa.ci.brainquizzz
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Review : AppCompatActivity() {
+    //declare
+    private lateinit  var ReviewText: TextView
+    private lateinit var btnReset : Button
+    private lateinit var btnExit : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
