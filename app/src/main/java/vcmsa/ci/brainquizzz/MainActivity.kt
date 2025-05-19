61ae16ff8db244b5462b4ba2d0adb92f744193f2
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 14:37:24 +0200

    initialize

--- app/src/main/java/vcmsa/ci/brainquizzz/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        //initialize the buttons
+        btnStart = findViewById<Button>(R.id.btnStart)
+        btnExit = findViewById<Button>(R.id.btnExit)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
