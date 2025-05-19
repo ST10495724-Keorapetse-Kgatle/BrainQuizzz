445240ffc61bfa2ee7c57e3b5669b58c65b45a73
ST10495724-Keorapetse-Kgatle <ST10495724@vcconnect.edu.za>
2025-05-19 14:50:14 +0200

    put my questions in an array

--- app/src/main/java/vcmsa/ci/brainquizzz/quizkit.kt
+++ app/src/main/java/vcmsa/ci/brainquizzz/quizkit.kt
@@ -16,6 +16,19 @@ class quizkit : AppCompatActivity() {
     private lateinit var btnFalse: Button
     private lateinit var btnNext: Button
     private lateinit var btnResult: Button
+    companion object {
+        val questions = arrayOf(
+            "Bananas are berries, but strawberries are not berries", // True
+            "The human brain stops growing once you reach adulthood", // False
+            "You can't hum while holding your nose", // True
+            "The color pink does not actually exist in the visible spectrum", // True
+            "The sun is yellow", // False
+            "HTML is a programming language", // False
+            "An octopus has 3 hearts" // True
+        )
+        val feedbackArray = booleanArrayOf(true, false, true, true, false, false, true)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
